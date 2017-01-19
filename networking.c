69bd35c59684c9a66722473b6483ea1f085415c3
Oliver Chen <ochen1@stuy.edu>
2017-01-18 23:52:50 -0500

    Update networking.c

--- networking.c
+++ networking.c
@@ -40,7 +40,7 @@ int server_setup() {
   su.val = 2;
   semctl(semid,0,SETVAL,su);
    int shmkey = ftok("makefile",6);
-  int shmid = shmget(key,2*sizeof(int),0644|IPC_CREAT);
+  int shmid = shmget(key,sizeof(int),0644|IPC_CREAT);
   return sd;
 
 }
